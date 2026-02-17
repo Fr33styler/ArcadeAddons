3b4238dc557d8d8dda4155002603085f6eb6ab65
Fr33styler <Fr33stylerMC@gmail.com>
2026-02-17 22:45:07 +0200

    Added description annotation

--- ResetScaleOnJoin/src/main/java/ro/fr33styler/arcade/addon/resetscaleonjoin/ResetScaleOnJoin.java
+++ ResetScaleOnJoin/src/main/java/ro/fr33styler/arcade/addon/resetscaleonjoin/ResetScaleOnJoin.java
@@ -4,8 +4,10 @@ import org.bukkit.attribute.Attribute;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import ro.fr33styler.gameengine.api.addon.Addon;
+import ro.fr33styler.gameengine.api.addon.annotation.Description;
 import ro.fr33styler.gameengine.api.event.game.player.GameJoinEvent;
 
+@Description(id = "ResetScaleOnJoin", author = "Fr33styler", version = "1.0")
 public class ResetScaleOnJoin extends Addon implements Listener {
 
     @Override
