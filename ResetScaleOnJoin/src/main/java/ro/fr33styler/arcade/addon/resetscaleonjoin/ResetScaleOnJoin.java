78695e85856fbb52cb31ff8e70b320db9881383c
Fr33styler <Fr33stylerMC@gmail.com>
2026-02-17 22:42:31 +0200

    Added ResetScaleOnJoin addon

---
+++ ResetScaleOnJoin/src/main/java/ro/fr33styler/arcade/addon/resetscaleonjoin/ResetScaleOnJoin.java
@@ -0,0 +1,26 @@
+package ro.fr33styler.arcade.addon.resetscaleonjoin;
+
+import org.bukkit.attribute.Attribute;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import ro.fr33styler.gameengine.api.addon.Addon;
+import ro.fr33styler.gameengine.api.event.game.player.GameJoinEvent;
+
+public class ResetScaleOnJoin extends Addon implements Listener {
+
+    @Override
+    public void onLoad() {
+        getEngine().getAddonManager().registerListener(this);
+    }
+
+    @Override
+    public void onUnload() {
+        getEngine().getAddonManager().unregisterListener(this);
+    }
+
+    @EventHandler
+    public void onJoin(GameJoinEvent event) {
+        event.getPlayer().getAttribute(Attribute.SCALE).setBaseValue(1);
+    }
+
+}
